858cc42271f4043f695956a98c1acbbc9ea01239
Samuel Gonzalez <xame2005@gmail.com>
2022-04-13 00:42:36 -0500

    [CREATED]Modal component

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -21,7 +21,43 @@ class App extends Component {
       <div className="container">
         <div className="row">
           {this.state.rockets.map((rocket) => (
-            <Modal rocket={rocket} />
+            <>
+              <Modal rocket={rocket} />
+              <div className="modal" tabindex="-1" role="dialog" id={`popup`}>
+                <div className="modal-dialog" role="document">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Modal title
+                      </h5>
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <p>Modal body text goes here.</p>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button type="button" className="btn btn-primary">
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
           ))}
         </div>
       </div>
